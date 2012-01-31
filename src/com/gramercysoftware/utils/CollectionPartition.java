dcc1d8a78f48b55028133c236267051b76766d6a
David Harcombe <david.harcombe@gmail.com>
2012-01-31 11:29:35 -0500

    Add GPL

--- src/com/gramercysoftware/utils/CollectionPartition.java
+++ src/com/gramercysoftware/utils/CollectionPartition.java
@@ -1,29 +1,48 @@
 package com.gramercysoftware.utils;
 
+/*
+ * Collection Partitioner
+ * Copyright (C) 2011 David Harcombe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import java.util.AbstractList;
 import java.util.List;
 
 public class CollectionPartition {
-
 	/**
-	 * * Returns consecutive {@linkplain List#subList(int, int) sublists} of a
-	 * list, * each of the same size (the final list may be smaller). For
-	 * example, * partitioning a list containing {@code [a, b, c, d, e]} with a
-	 * partition * size of 3 yields {@code [[a, b, c], [d, e]]} -- an outer list
-	 * containing * two inner lists of three and two elements, all in the
-	 * original order. * *
+	 * 
+	 * Returns consecutive {@linkplain List#subList(int, int) sublists} of a
+	 * list, 
+	 * each of the same size (the final list may be smaller). For
+	 * example, 
+	 * partitioning a list containing {@code [a, b, c, d, e]} with a
+	 * partition size of 3 yields {@code [[a, b, c], [d, e]]} -- an outer list
+	 * containing two inner lists of three and two elements, all in the
+	 * original order. 
 	 * <p>
-	 * The outer list is unmodifiable, but reflects the latest state of the *
-	 * source list. The inner lists are sublist views of the original list, *
+	 * The outer list is unmodifiable, but reflects the latest state of the
+	 * source list. The inner lists are sublist views of the original list,
 	 * produced on demand using {@link List#subList(int, int)}, and are subject
-	 * * to all the usual caveats about modification as explained in that API. *
-	 * * * Adapted from http://code.google.com/p/google-collections/ * * @param
-	 * list the list to return consecutive sublists of * @param size the desired
-	 * size of each sublist (the last may be * smaller) * @return a list of
-	 * consecutive sublists * @throws IllegalArgumentException if
-	 * {@code partitionSize} is nonpositive *
+	 * to all the usual caveats about modification as explained in that API.
+	 * 
+	 * Adapted from http://code.google.com/p/google-collections/ 
+	 * @param list the list to return consecutive sublists of 
+	 * @param size the desired size of each sublist (the last may be smaller) 
+	 * @return a list of consecutive sublists 
+	 * @throws IllegalArgumentException if {@code partitionSize} is nonpositive
 	 */
-
 	public static <T> List<List<T>> partition(List<T> list, int size) {
 		if (list == null)
 			throw new NullPointerException("'list' must not be null");
--- src/com/gramercysoftware/utils/StringUtils.java
+++ src/com/gramercysoftware/utils/StringUtils.java
@@ -1,5 +1,22 @@
 package com.gramercysoftware.utils;
 
+/*
+ * StringUtils
+ * Copyright (C) 2011 David Harcombe
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class StringUtils {
 
 	private static final String EMPTY = "";
@@ -7,7 +24,7 @@ public class StringUtils {
 	public static String center(String original, int finalSize) {
 		return center(original, finalSize, " ");
 	}
-	
+
 	public static String center(String original, int finalSize, String padder) {
 		int padSize = finalSize - original.length();
 		int leftPad = padSize / 2;
@@ -18,7 +35,7 @@ public class StringUtils {
 	public static String leftPad(String original, int leftPad) {
 		return leftPad(original, leftPad, " ");
 	}
-	
+
 	public static String leftPad(String original, int leftPad, String padder) {
 		return repeat(leftPad, padder).concat(original);
 	}
@@ -26,49 +43,49 @@ public class StringUtils {
 	public static String rightPad(String original, int rightPad) {
 		return rightPad(original, rightPad, " ");
 	}
-	
+
 	public static String rightPad(String original, int rightPad, String padder) {
 		return original.concat(repeat(rightPad, padder));
 	}
-	
+
 	public static String repeat(int repeatCount, String padder) {
 		StringBuffer padding = new StringBuffer();
-		for(int i = 0; i < repeatCount; i++) {
+		for (int i = 0; i < repeatCount; i++) {
 			padding.append(padder);
 		}
 		return padding.toString();
 	}
 
-    public static String substringBefore(String str, String separator) {
-        if (isEmpty(str) || separator == null) {
-            return str;
-        }
-        if (separator.length() == 0) {
-            return EMPTY;
-        }
-        int pos = str.indexOf(separator);
-        if (pos == -1) {
-            return str;
-        }
-        return str.substring(0, pos);
-    }
+	public static String substringBefore(String str, String separator) {
+		if (isEmpty(str) || separator == null) {
+			return str;
+		}
+		if (separator.length() == 0) {
+			return EMPTY;
+		}
+		int pos = str.indexOf(separator);
+		if (pos == -1) {
+			return str;
+		}
+		return str.substring(0, pos);
+	}
 
-    public static String substringBeforeLast(String str, String separator) {
-        if (isEmpty(str) || isEmpty(separator)) {
-            return str;
-        }
-        int pos = str.lastIndexOf(separator);
-        if (pos == -1) {
-            return str;
-        }
-        return str.substring(0, pos);
-    }
+	public static String substringBeforeLast(String str, String separator) {
+		if (isEmpty(str) || isEmpty(separator)) {
+			return str;
+		}
+		int pos = str.lastIndexOf(separator);
+		if (pos == -1) {
+			return str;
+		}
+		return str.substring(0, pos);
+	}
 
-    public static boolean isEmpty(String str) {
-        return str == null || str.length() == 0;
-    }
+	public static boolean isEmpty(String str) {
+		return str == null || str.length() == 0;
+	}
 
-    public static boolean isNotEmpty(String str) {
-    	return !isEmpty(str);
-    }
+	public static boolean isNotEmpty(String str) {
+		return !isEmpty(str);
+	}
 }
